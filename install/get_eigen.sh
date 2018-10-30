21039b5beee9fc2edf2d06556d5c59d0c54982df
Connor Glosser <glosser1@gmail.com>
2018-10-29 22:04:40 -0400

    lol

--- install/get_eigen.sh
+++ install/get_eigen.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-wget https://bitbucket.org/eigen/eigen/get/3.3.2.tar.gz -O /tmp/eigen.tar.gz
-mkdir -p /tmp/eigen/build && tar -xvzf /tmp/eigen.tar.gz -C eigen --strip-components 1
-cd /tmp/eigen/build && cmake .. && make install
+cd /tmp
+wget https://bitbucket.org/eigen/eigen/get/3.3.2.tar.gz -O eigen.tar.gz
+mkdir -p eigen/build && tar -xvzf eigen.tar.gz -C eigen --strip-components 1
+cd eigen/build && cmake .. && make install
